598cab8729a49979aed53e73c9e411119f8e03f4
thedatadev <thedatadev@gmail.com>
2019-06-23 10:17:44 +1000

    init server

---
+++ server/db/models.js
@@ -0,0 +1,46 @@
+const models = [
+    
+    {
+        name: "chair",
+        uri: "https://cdn.glitch.com/56c001f0-5c8d-45df-a008-23bbde1d84a4%2Fchair.glb",
+        position: { x: 0, y: 0, z: 0 },
+        rotation: { x: 0, y: 0, z: 0 },
+        scale:    { x: 0, y: 0, z: 0 },
+    },
+
+    {
+        name: "bookshelf",
+        uri: "https://cdn.glitch.com/56c001f0-5c8d-45df-a008-23bbde1d84a4%2Fbookshelf.glb",
+        position: { x: 0, y: 0, z: 0 },
+        rotation: { x: 0, y: 0, z: 0 },
+        scale:    { x: 0, y: 0, z: 0 },
+    },
+
+    {
+        name: "floor lamp",
+        uri: "https://cdn.glitch.com/56c001f0-5c8d-45df-a008-23bbde1d84a4%2Ffloor-lamp.glb",
+        position: { x: 0, y: 0, z: 0 },
+        rotation: { x: 0, y: 0, z: 0 },
+        scale:    { x: 0, y: 0, z: 0 },
+    },
+
+    {
+        name: "table",
+        uri: "https://cdn.glitch.com/56c001f0-5c8d-45df-a008-23bbde1d84a4%2Ftable.glb",
+        position: { x: 0, y: 0, z: 0 },
+        rotation: { x: 0, y: 0, z: 0 },
+        scale:    { x: 0, y: 0, z: 0 },
+    },
+
+    {
+        name: "bed",
+        uri: "https://cdn.glitch.com/56c001f0-5c8d-45df-a008-23bbde1d84a4%2Fbed.glb",
+        position: { x: 0, y: 0, z: 0 },
+        rotation: { x: 0, y: 0, z: 0 },
+        scale:    { x: 0, y: 0, z: 0 },
+    },
+
+];
+
+
+export default models;
