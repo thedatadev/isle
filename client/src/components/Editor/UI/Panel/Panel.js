db6277bc2a8e5795a17596fc3541a8bbd45bc246
thedatadev <thedatadev@gmail.com>
2019-06-24 20:30:16 +1000

    enter vr button event listener

--- client/src/components/Editor/UI/Panel/Panel.js
+++ client/src/components/Editor/UI/Panel/Panel.js
@@ -1,6 +1,6 @@
 
 // Dependencies
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
 
 // GraphQL
@@ -11,78 +11,118 @@ import { getRoomByID } from '../../../../api/graphql/queries';
 // Styling
 import './Panel.css'
 
+function Header() {
+    
+    return (
+
+        <div id="panel-header">
+
+            <p>Panel Header</p>
+
+            {/* Save */}
+
+            {/* Minimize */}
+
+            {/* Settings */}
+
+        </div>
+
+    );
+
+}
+
+
+
+
+function Palette(props) {
+    
+    if ( props.models && props.models.length > 0 ) {
+
+        return (
+
+            <div id="palette">
+
+                <Models models={props.models} addObject={props.addObject}/>
+
+            </div>
+
+        );
+
+    } else {
+
+        return (
+        
+            <div id="empty-palette">No models to display.</div>
+            
+        );
+
+    }
+
+}
+
+function Models(props) {
+
+    return props.models.map(model => {
+
+        return (
+
+            <div className="model"
+                 key={model.id} 
+                 onClick={ _ => props.addObject(model) }>
+
+                {model.name}
+
+            </div>
+
+        );
+
+    });
+
+}
+
+
+function EnterVR() {
+
+    function enterVRMode(event) {
+
+        event.preventDefault();
+        event.stopImmediatePropagation(); // https://stackoverflow.com/questions/12052132/jquery-mobile-click-event-binding-twice
+
+        console.log("Entering VR mode!")
+
+    }
+
+    let btn = useRef(null);
+
+    useEffect(() => {
+
+        btn.addEventListener("click", enterVRMode);
+
+        return function() {
+
+            if (btn !== null) btn.removeEventListener("click", enterVRMode);
+
+        }
+
+    });
+
+
+    return (
+
+        <div id="enter-vr">
+
+            <i className="fas fa-vr-cardboard" ref={ element => btn = element }></i>
+
+        </div>
+
+    );
+
+
+}
 
 
 function Panel(props) {
 
-
-    function Models(props) {
-
-        return props.models.map(model => {
-    
-            return (
-    
-                <div className="model"
-                     key={model.id} 
-                     onClick={ _ => props.addObject(model) }>
-    
-                    {model.name}
-    
-                </div>
-    
-            );
-    
-        });
-    
-    }
-    
-    
-    function Palette(props) {
-    
-        if ( props.models && props.models.length > 0 ) {
-    
-            return (
-    
-                <div id="palette">
-    
-                    <Models models={props.models} addObject={props.addObject}/>
-    
-                </div>
-    
-            );
-    
-        } else {
-    
-            return (
-            
-                <div id="empty-palette">No models to display.</div>
-                
-            );
-    
-        }
-    
-    }
-    
-    
-    function Header() {
-    
-        return (
-    
-            <div id="panel-header">
-    
-                <p>Panel Header</p>
-    
-                {/* Save */}
-    
-                {/* Minimize */}
-    
-                {/* Settings */}
-    
-            </div>
-    
-        );
-    
-    }
 
     function models() {
 
@@ -101,6 +141,8 @@ function Panel(props) {
             <Header />
 
             <Palette models={models()} addObject={props.addObject}/>
+
+            <EnterVR />
 
         </div>
 
